9c2a680ff3a1639dbaea4d7a622a687ae1de9f0f
Abhinav Rakhunde <rakhundeabhinav@gmail.com>
2025-09-29 23:47:58 +0530

    Create BigONotation.cpp

---
+++ BigONotation.cpp
@@ -0,0 +1,24 @@
+//Abhinav Rakhunde
+//PRN:24070123001
+
+#include <iostream>
+using namespace std;
+
+void printAllPairs(int arr[], int n) {
+    for (int i = 0; i < n; i++) {          
+        for (int j = 0; j < n; j++) {      
+            cout << arr[i] << ", " << arr[j] << endl;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printAllPairs(arr, n);  
+}
+
+/* OUTPUT
+1, 1
+1, 2
+*/
